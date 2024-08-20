b715ef1f2977165996d0d86e6e82123e0e6e0fa3
Luís Medrado <131623138+LuisMedrado@users.noreply.github.com>
2024-08-20 17:00:17 -0300

    LeagueData Script version 1.0

--- LeagueData_V1.0.py
+++ LeagueData_V1.0.py
@@ -1,3 +1,7 @@
+# LeagueData is not endorsed by Riot Games and does not reflect the views or opinions
+# of Riot Games or anyone officially involved in producing or managing Riot Games properties.
+# Riot Games and all associated properties are trademarks or registered trademarks of Riot Games, Inc
+
 import requests, pymysql
 
 # INSERIR OS DADOS DA INSTÂNCIA DE BANCO NOS CAMPOS ABAIXO.
